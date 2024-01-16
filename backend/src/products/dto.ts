20110e7e1964a6a1d9523c9fe81c33504ce7b30e
tamani phiri <tamanigabriel0@gmail.com>
2024-01-16 23:46:20 +0200

    c

--- backend/src/products/dto.ts
+++ backend/src/products/dto.ts
@@ -1,17 +1,45 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  IsArray,
+  ArrayMinSize,
+  Min,
+  IsPositive,
+} from "class-validator";
+
 export class ProductDTO {
-    name: string;
-    description: string;
-    price: number;
-    stock?: number;
-    imageUrl: string[];
-    categoryId: number;
-  
-    constructor(data: ProductDTO) {
-      this.name = data.name;
-      this.description = data.description;
-      this.price = data.price;
-      this.stock = data.stock;
-      this.imageUrl = data.imageUrl;
-      this.categoryId = data.categoryId;
-    }
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  readonly price: number;
+
+  @IsNumber()
+  @IsPositive()
+  readonly stock?: number;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  readonly imageUrl: string[];
+
+  @IsNotEmpty()
+  @IsNumber()
+  readonly categoryId: number;
+
+  constructor(data: ProductDTO) {
+    this.name = data.name;
+    this.description = data.description;
+    this.price = data.price;
+    this.stock = data.stock;
+    this.imageUrl = data.imageUrl;
+    this.categoryId = data.categoryId;
   }
+}
