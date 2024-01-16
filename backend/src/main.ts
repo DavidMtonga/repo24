393f701660d8417665f71b966ae341797d236167
tamani phiri <tamanigabriel0@gmail.com>
2024-01-16 23:55:32 +0200

    c

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express, { Application, Response } from "express";
 import compression from "compression";
 import "dotenv/config";
+import morgan from "morgan";
 
 const app: Application = express();
 const port = process.env.PORT || 5000;
@@ -10,8 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(compression());
-
-
+app.use(morgan("dev"));
 
 app.get("/", (_, res: Response) => {
   res.send("API up");
