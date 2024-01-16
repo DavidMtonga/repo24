e5cf0afdef2c9d3ca505ef732ef5f50520c037ba
DavidMtonga <dmtonga840@gmail.com>
2024-01-16 12:24:24 +0200

    changes

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import MobNavbar from "./components/MobNavbar"
+import Navbar from "./components/Navbar"
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <MobNavbar />
+    </main>
+  )
+}
+
+export default App
