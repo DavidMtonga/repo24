dce30926917e603dcbbc363796a3d57a07fee8ba
tamani phiri <tamanigabriel0@gmail.com>
2024-01-16 22:53:34 +0200

    c

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 app.use(compression());
 
 app.get("/", (_, res: Response) => {
