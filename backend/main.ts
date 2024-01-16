4258499b59e8794669d6f06d08abd7f312e39a86
tamani phiri <tamanigabriel0@gmail.com>
2024-01-16 20:30:03 +0200

    c

--- backend/main.ts
+++ backend/main.ts
@@ -1,6 +1,9 @@
 import cors from "cors";
 import express, { Express } from "express";
+import compression from "compression";
 
 const app: Express = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(compression());
