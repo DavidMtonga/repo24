830806e86110436a6888d7e6554e5b01a780eaf2
tamani phiri <tamanigabriel0@gmail.com>
2024-01-18 00:15:44 +0200

    c

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,8 @@ import compression from "compression";
 import "dotenv/config";
 import morgan from "morgan";
 import productRouter from "./products/router";
+import cookieParser from "cookie-parser";
+import authRouter from "./user/router";
 
 const app: Application = express();
 const port = process.env.PORT || 5000;
@@ -11,10 +13,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(compression());
 app.use(morgan("dev"));
 
 app.use("/api/product", productRouter);
+app.use("/api/user", authRouter);
 
 app.get("/", (_, res: Response) => {
   res.send("API up");
