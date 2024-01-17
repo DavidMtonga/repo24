7af8bf7c95b455057675c8924ebe57aad1efa1d7
tamani phiri <tamanigabriel0@gmail.com>
2024-01-17 23:33:20 +0200

    c

--- backend/src/user/dto.ts
+++ backend/src/user/dto.ts
@@ -64,4 +64,37 @@ export class UserIdDTO {
   }
 }
 
-export class UserUpdateDTO {}
+export class UserUpdateDTO {
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  @Min(0)
+  readonly id: number;
+
+  @IsNotEmpty()
+  @Min(6)
+  @Max(20)
+  readonly username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @Min(3)
+  readonly email: string;
+
+  @IsNotEmpty()
+  @Min(6)
+  @Max(20)
+  readonly password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly address?: string;
+
+  constructor(data: UserUpdateDTO) {
+    this.id = data.id;
+    this.username = data.username;
+    this.email = data.email;
+    this.password = data.password;
+    this.address = data.address;
+  }
+}
