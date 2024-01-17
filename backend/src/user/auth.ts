bd5f5a0de3821ee31fe121cc1f657cbf5fba8130
tamani phiri <tamanigabriel0@gmail.com>
2024-01-18 00:55:06 +0200

    c

--- backend/src/user/auth.ts
+++ backend/src/user/auth.ts
@@ -12,33 +12,40 @@ export class AuthController {
     try {
       const userLoginDto = new UserLoginDTO(req.body);
       const userErrors = await validate(userLoginDto);
+
       if (userErrors.length > 0) {
         return res.status(StatusCodes.CONFLICT).json({
           error: userErrors.map((e) => e.constraints),
         });
       }
+
       const user = await prisma.user.findFirst({
         where: {
           email: userLoginDto.email,
         },
       });
+
       if (!user) {
         return res.status(StatusCodes.BAD_REQUEST).json({
           error: "User doesn't exist",
         });
       }
+
       const match = await compare(userLoginDto.password, user.password);
       if (!match) {
         return res.status(StatusCodes.CONFLICT).json({
           error: "Passwords don't match",
         });
       }
+
       const payload = {
         sub: user.id,
         username: user.username,
         email: user.email,
       };
+
       const token = jwt.sign(payload, `${process.env.JWT_SECRET}`);
+      
       return res
         .cookie("token", token, {
           httpOnly: true,
