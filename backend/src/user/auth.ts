f19c3c864a81145b772c743c7b960d7db016dc1f
tamani phiri <tamanigabriel0@gmail.com>
2024-01-18 00:00:57 +0200

    c

--- backend/src/user/auth.ts
+++ backend/src/user/auth.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import { UserLoginDTO } from "./dto";
 import { validate } from "class-validator";
 import { StatusCodes } from "http-status-codes";
+import { prisma } from "../config/db";
+import { compare } from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export class AuthController {
   async Login(req: Request, res: Response) {
@@ -13,6 +16,35 @@ export class AuthController {
           error: userErrors.map((e) => e.constraints),
         });
       }
+      const user = await prisma.user.findFirst({
+        where: {
+          email: userLoginDto.email,
+        },
+      });
+      if (!user) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          error: "User doesn't exist",
+        });
+      }
+      const match = await compare(userLoginDto.password, user.password);
+      if (!match) {
+        return res.status(StatusCodes.CONFLICT).json({
+          error: "Passwords don't match",
+        });
+      }
+      const payload = {
+        sub: user.id,
+        username: user.username,
+        email: user.email,
+      };
+      const token = jwt.sign(payload, `${process.env.JWT_SECRET}`, {
+        expiresIn: "1hr",
+      });
+      return res.status(StatusCodes.OK).json({
+        username: user.username,
+        email: user.email,
+        token: token,
+      });
     } catch (error) {}
   }
 }
