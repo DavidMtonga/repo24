7e615bae7de507a75e2ebdf0be7afb59e2ce901e
tamani phiri <tamanigabriel0@gmail.com>
2024-01-17 23:21:14 +0200

    c

---
+++ backend/src/user/dto.ts
@@ -0,0 +1,24 @@
+import { IsNotEmpty, IsEmail, Min, Max } from "class-validator";
+
+export class UserLoginDTO {
+  @IsEmail()
+  @IsNotEmpty()
+  @Min(3)
+  readonly email: string;
+
+  @IsNotEmpty()
+  @Min(6)
+  @Max(20)
+  readonly password: string;
+
+  constructor(data: UserLoginDTO) {
+    this.email = data.email;
+    this.password = data.password;
+  }
+}
+
+export class UserRegisterDTO {}
+
+export class UserIdDTO {}
+
+export class UserUpdateDTO {}
