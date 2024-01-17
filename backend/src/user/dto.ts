6f4977763bbbcd989bf4f6c300d3cdb56fe88f7b
tamani phiri <tamanigabriel0@gmail.com>
2024-01-18 00:22:19 +0200

    c

--- backend/src/user/dto.ts
+++ backend/src/user/dto.ts
@@ -10,12 +10,10 @@ import {
 export class UserLoginDTO {
   @IsEmail()
   @IsNotEmpty()
-  @Min(3)
   readonly email: string;
 
   @IsNotEmpty()
   @Min(6)
-  @Max(20)
   readonly password: string;
 
   constructor(d: UserLoginDTO) {
