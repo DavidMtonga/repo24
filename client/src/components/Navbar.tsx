763e977b8577490c89bfbaaa420871ab49b8e5b9
tamani phiri <tamanigabriel0@gmail.com>
2024-01-28 20:42:05 +0200

    c

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,29 +5,24 @@ import CartCountBadge from "./CartCountBadge";
 const Navbar = () => {
   return (
     <div className=" sticky top-0 z-10">
-      <div className="px-4 md:px-12">
-        <div className="flex justify-between items-center gap-12">
-          <h1 className=" text-4xl text-red-600 font-medium">Logo</h1>
-          <div className=" relative w-full">
-            <input
-              className="outline-none border-gray-500 px-6 py-3 bg-inherit border rounded-[30px] w-full"
-              type="text"
-              placeholder="Search product..."
-            />
-            <BsSearch
-              className=" absolute top-0 right-0 mt-4 mr-5 text-gray-500"
-              size={20}
-            />
-          </div>
+      <div className="flex justify-between items-center gap-12 px-4 md:px-12 py-5">
+        <h1 className="text-2xl text-red-600 font-bold">Retaila</h1>
+        <div className="relative w-full max-w-sm border border-gray-500 rounded-full p-3 md:flex hidden items-center">
+          <input
+            className="outline-none bg-inherit flex-1 "
+            type="text"
+            placeholder="Search product..."
+          />
+          <BsSearch className=" text-gray-500" size={20} />
+        </div>
 
-          <div className="flex gap-4">
-            <div className="icon__wrapper">
-              <AiOutlineUser />
-            </div>
-            <div className="icon__wrapper relative">
-              <AiOutlineShoppingCart />
-              <CartCountBadge size=" w-[25px] h-[25px]" />
-            </div>
+        <div className="flex gap-4">
+          <div className="icon__wrapper">
+            <AiOutlineUser />
+          </div>
+          <div className="icon__wrapper relative">
+            <AiOutlineShoppingCart />
+            <CartCountBadge size=" w-[25px] h-[25px]" />
           </div>
         </div>
       </div>
