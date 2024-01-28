82bb8d9e224f44e0e630bde419450b885ac92200
tamani phiri <tamanigabriel0@gmail.com>
2024-01-28 21:10:20 +0200

    c

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import pic2 from "../assets/images/pic2.jpg";
+import bg from "../assets/images/hero.jpg";
 import { BsArrowRight } from "react-icons/bs";
 import pic4 from "../assets/images/pic4.jpg";
 import pic3 from "../assets/images/pic3.jpg";
@@ -6,30 +6,26 @@ import pic3 from "../assets/images/pic3.jpg";
 const Hero = () => {
   return (
     <div className="container py-16 min-h-screen">
-      <div className=" grid xl:grid-cols-3 xl:grid-rows-2 gap-8">
-        <div className=" xl:col-span-2 xl:row-start-1 xl:row-end-[-1] relative">
-          <img
-            className=" w-full h-full object-cover rounded-lg"
-            src={pic2}
-            alt="hero_image"
-          />
-          <div className="absolute max-w-[470px] sm:ml-16 ml-8 top-[50%] -translate-y-[50%] sm:space-y-4">
-            <p className="text-2xl hidden sm:block text-blue-400">
-              {" "}
-              100% Original Apple headphones
-            </p>
-            <h2 className=" text-2xl sm:text-4xl md:text-6xl font-bold text-blue-400">
-              {" "}
-              High quality products!
-            </h2>
-            <p className=" text-gray-500 text-xl pt-4 sm:pt-8"> Starting At</p>
-            <div className="font-medium text-red-600 text-2xl sm:text-4xl pb-4 sm:bp-8">
-              {" "}
-              $18.39
-            </div>
-            <div className=" bg-accentDark hover:bg-accent text-white rounded-full w-fit flex items-center gap-4 px-4 py-2 text-[14px] sm:px-6 sm:py-3 cursor-pointer">
-              Shop Now <BsArrowRight />
-            </div>
+      <div className="grid xl:grid-cols-3 xl:grid-rows-2 gap-8">
+        <div
+          className=" bg-cover bg-center xl:col-span-2 p-4 md:p-12 xl:row-start-1 xl:row-end-[-1]"
+          style={{ backgroundImage: `url(${bg})` }}
+        >
+          <p className="text-2xl hidden backdrop-blur w-fit sm:block">
+            {" "}
+            100% Original Apple headphones
+          </p>
+          <h2 className=" text-2xl sm:text-4xl md:text-6xl font-bold">
+            {" "}
+            High quality products!
+          </h2>
+          <p className=" text-gray-500 text-xl pt-4 sm:pt-8"> Starting At</p>
+          <div className=" text-red-600 font-bold text-2xl sm:text-4xl pb-4 sm:bp-8">
+            {" "}
+            K18.39
+          </div>
+          <div className=" bg-accentDark hover:bg-accent text-white rounded-full w-fit flex items-center gap-4 px-4 py-2 text-[14px] sm:px-6 sm:py-3 cursor-pointer">
+            Shop Now <BsArrowRight />
           </div>
         </div>
         <div className="relative">
