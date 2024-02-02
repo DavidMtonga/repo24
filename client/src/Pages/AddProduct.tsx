be53a749914814428848d7a9abb15786a5f401a2
tamani phiri <tamanigabriel0@gmail.com>
2024-02-02 12:57:11 +0200

    |

--- client/src/Pages/AddProduct.tsx
+++ client/src/Pages/AddProduct.tsx
@@ -1,9 +1,37 @@
+import { ref, uploadBytes } from "firebase/storage";
+import { useState } from "react";
+import { storage } from "../../firebase/config";
+
 const AddProduct = () => {
+  const [images, setImages] = useState<FileList | null>(null);
+
+  const uploadImages = async () => {
+    if (!images) {
+      return alert("Please select one ore more images");
+    }
+    for (let i = 0; i < images?.length; i++) {
+      const imageRef = ref(storage, `/multi/${images[i].name}`);
+
+      const result = await uploadBytes(imageRef, images[i])
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+
+      console.log(result);
+    }
+  };
+
+  console.log(images);
+
   return (
     <div>
-      her
+      <input type="file" multiple onChange={(e) => setImages(e.target.files)} />
+      <button onClick={uploadImages}>Upload</button>
     </div>
-  )
-}
+  );
+};
 
-export default AddProduct
+export default AddProduct;
